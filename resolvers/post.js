4ac4d0edc8bc83052822ef80c824abbf912e9553
Amartya Mondal <atm1504.in@gmail.com>
2020-10-19 13:33:46 +0530

    Add posts

--- resolvers/post.js
+++ resolvers/post.js
@@ -1,9 +1,26 @@
 const { gql } = require("apollo-server-express");
+const { posts } = require("./../temp");
 
-const totalPosts= () => 1504;
+const totalPosts= () => posts.length;
+const allPosts = () => posts;
 
+// Mutation
+const newPost = (parent, args) => {
+    console.log(args);
+    const post = {
+        id: posts.length+1,
+        title: args.title,
+        description: args.description
+    }
+
+    posts.push(post);
+    return post;
+}
 module.exports = {
     Query: {
-        totalPosts
+        totalPosts,allPosts
+    },
+    Mutation: {
+        newPost
     }
 };
--- typeDefs/post.js
+++ typeDefs/post.js
@@ -2,7 +2,18 @@ const { gql } = require("apollo-server-express");
 
 
 module.exports = gql`
+type Post{
+    id:ID!
+    title:String!
+    description:String!
+}
 type Query{
     totalPosts: Int!
+    allPosts: [Post!]!
 }
+# mutation
+type Mutation{
+    newPost(title:String!, description:String!): Post!
+}
+
 `;
